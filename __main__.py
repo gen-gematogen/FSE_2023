ef3894ca2fee07a6e58504d77f62e2261d961b5b
Oleg Kobzarev <o.kobzarev@ispras.ru>
2023-10-29 16:43:36 +0300

    Some fixes

--- __main__.py
+++ __main__.py
@@ -1,4 +1,3 @@
-
 from utils.bot import *
 
 desk = Desk()
@@ -10,10 +9,6 @@ The only command is a /restart
 
 API_TOKEN = "" # YOUR API TOKEN HERE
 bot = TeleBot(API_TOKEN, parse_mode=None)
-
-
-def restart(chat_id):
-    save_state(chat_id, Desk(), False, (0, 0))
 
 
 try:
@@ -49,46 +44,62 @@ def game_restart(message):
     create_base_response(bot, message, moves.keys())
 
 
+def move_logic(message, desk, attack_pos):
+    attack, moves = desk.check_available_moves()
+    target_pos = moves[attack_pos]
+    move_x, move_y = decode_pos(message.text)
+    if (move_x, move_y) not in target_pos:
+        bot.send_message(chat_id=message.chat.id, text="Wrong move, use buttons")
+        create_base_response(bot, message, target_pos)
+        return
+    x_pos, y_pos = attack_pos
+    attacked = desk.do_move(attack_pos, (move_x, move_y), desk.get_cell_state(x_pos, y_pos))
+    x_pos = move_x
+    y_pos = move_y
+    attack, moves = desk.check_available_moves()
+    if len(moves.get((x_pos, y_pos), [])) == 0 or not attack:
+        if attacked:
+            desk.change_turn()
+            attack, moves = desk.check_available_moves()
+        attacked = False
+    if attacked:
+        print("You attacked, you can move more")
+    save_state(message.chat.id, desk, attacked, (x_pos, y_pos))
+    image = desk.create_image_from_desk_state()
+    image.save("user_data/" + str(message.chat.id) + "/desk.png")
+    if attacked:
+        create_base_response(bot, message, moves[(x_pos, y_pos)])
+    else:
+        create_base_response(bot, message, moves.keys())
+
+
+def choosing_piece_logic(message, desk):
+    attack, moves = desk.check_available_moves()
+    x_pos, y_pos = decode_pos(message.text)
+    if (x_pos, y_pos) not in moves.keys():
+        bot.send_message(chat_id=message.chat.id, text="Wrong move, use buttons")
+        create_base_response(bot, message, moves.keys())
+        return
+    save_state(message.chat.id, desk, True, (x_pos, y_pos))
+    image = desk.create_image_from_desk_state()
+    image.save("user_data/" + str(message.chat.id) + "/desk.png")
+    create_base_response(bot, message, moves[(x_pos, y_pos)])
+
+
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
     desk, is_move_stage, attack_pos = get_state(message.chat.id)
     if is_move_stage:
-        attack, moves = desk.check_available_moves()
-        target_pos = moves[attack_pos]
-        move_x, move_y = decode_pos(message.text)
-        if (move_x, move_y) not in target_pos:
-            bot.send_message(chat_id=message.chat.id, text="Wrong move, use buttons")
-            create_base_response(bot, message, target_pos)
-            return
-        x_pos, y_pos = attack_pos
-        attacked = desk.do_move(attack_pos, (move_x, move_y), desk.get_cell_state(x_pos, y_pos))
-        x_pos = move_x
-        y_pos = move_y
-        attack, moves = desk.check_available_moves()
-        if len(moves.get((x_pos, y_pos), [])) == 0 or not attack:
-            if attacked:
-                desk.change_turn()
-            attacked = False
-        if attacked:
-            print("You attacked, you can move more")
-        save_state(message.chat.id, desk, attacked, (x_pos, y_pos))
-        image = desk.create_image_from_desk_state()
-        image.save("user_data/" + str(message.chat.id) + "/desk.png")
-        if attacked:
-            create_base_response(bot, message, moves[(x_pos, y_pos)])
-        else:
-            create_base_response(bot, message, moves.keys())
+        move_logic(message, desk, attack_pos)
     else:
-        attack, moves = desk.check_available_moves()
-        x_pos, y_pos = decode_pos(message.text)
-        if (x_pos, y_pos) not in moves.keys():
-            bot.send_message(chat_id=message.chat.id, text="Wrong move, use buttons")
-            create_base_response(bot, message, moves.keys())
-            return
-        save_state(message.chat.id, desk, True, (x_pos, y_pos))
-        image = desk.create_image_from_desk_state()
-        image.save("user_data/" + str(message.chat.id) + "/desk.png")
-        create_base_response(bot, message, moves[(x_pos, y_pos)])
+        choosing_piece_logic(message, desk)
+    desk, is_move_stage, attack_pos = get_state(message.chat.id)
+    w, b = desk.count_pieces()
+    if w == 0 or b == 0:
+        if w == 0:
+            bot.send_message(chat_id=message.chat.id, text="Black side won, if you want play more, press /restart")
+        if b == 0:
+            bot.send_message(chat_id=message.chat.id, text="White side won, if you want play more, press /restart")
 
 
 print("Bot started")
--- utils/bot.py
+++ utils/bot.py
@@ -1,10 +1,12 @@
 from telebot import TeleBot, types
 import os
 import pickle
+
 from utils.checkers import *
 
 
 parent_dir = os.path.abspath(__file__).replace('\\', '/').rsplit('/', 2)[0]
+
 
 def create_markup(pos_list):
     markup = types.ReplyKeyboardMarkup(row_width=3)
@@ -14,11 +16,25 @@ def create_markup(pos_list):
 
 
 def save_state(chat_id, desk, is_move_stage, attack_pos):
+    try:
+        os.mkdir("user_data/" + str(chat_id))
+    except Exception:
+        pass
     with open(parent_dir + "/user_data/" + str(chat_id) + "/game_state.pkl", 'wb') as game_data:
         pickle.dump((desk, is_move_stage, attack_pos), game_data)
 
 
+def restart(chat_id):
+    save_state(chat_id, Desk(), False, (0, 0))
+
+
 def get_state(chat_id):
+    try:
+        os.mkdir("user_data/" + str(chat_id))
+    except Exception:
+        pass
+    if not os.path.isfile(parent_dir + "/user_data/" + str(chat_id) + "/desk.png"):
+        restart(chat_id)
     with open(parent_dir + "/user_data/" + str(chat_id) + "/game_state.pkl", 'rb') as game_data:
         return pickle.load(game_data)
 
